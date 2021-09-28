a8d31fe730b41bc62c23eb5ca9ad237cf626dc47
chenlianghong <clh021@gmail.com>
2021-09-28 16:36:34 +0800

    test: add nginx proxy mutil by domain log

--- test/nginx.nossl.sh
+++ test/nginx.nossl.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # 请先进入 nginx 目录 启动服务 docker-compose up
-curl -v 'http://127.0.0.1:7903/lnks_homecloud.clh021.webshell.ShellService/Run2' \
+curl -v 'http://grpc21.localhost:7564/lnks_homecloud.clh021.webshell.ShellService/Run2' \
   -H 'x-grpc-web: 1' \
   -H 'content-type: application/grpc-web+proto' \
   --data-binary @data.bin \
   --compressed \
-  --output output2.log
+  --output output.grpc21.log
+curl -v 'http://grpc22.localhost:7564/lnks_homecloud.clh021.webshell.ShellService/Run2' \
+  -H 'x-grpc-web: 1' \
+  -H 'content-type: application/grpc-web+proto' \
+  --data-binary @data.bin \
+  --compressed \
+  --output output.grpc22.log
 # data.bin,output 输出请使用 cat 查看内容
