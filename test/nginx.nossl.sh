78325e84dbb6ed116a3cf472e900e21bac8c8ec6
chenlianghong <clh021@gmail.com>
2021-09-28 11:12:28 +0800

    test: fix shell "\ "

--- test/nginx.nossl.sh
+++ test/nginx.nossl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 请先进入 nginx 目录 启动服务 docker-compose up
-curl -v 'http://127.0.0.1:7903/lnks_homecloud.clh021.webshell.ShellService/Run2' \ 
+curl -v 'http://127.0.0.1:7903/lnks_homecloud.clh021.webshell.ShellService/Run2' \
   -H 'x-grpc-web: 1' \
   -H 'content-type: application/grpc-web+proto' \
   --data-binary @data.bin \
